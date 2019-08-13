217e03aa842a031a840983effa3c5c7139e7c36a
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-14 06:23:34 +0900

    Update Main.php

--- src/pawarenessc/mi/Main.php
+++ src/pawarenessc/mi/Main.php
@@ -13,7 +13,34 @@ use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
+use pocketmine\item\Item;
+
 class Main extends pluginBase implements Listener{
 
 	public function onEnable(){
+		$this->getLogger()->info("=========================");
+		$this->getLogger()->info("MoreInvを読み込みました");
+		$this->getLogger()->info("製作者 PawarenessC");
+		$this->getLogger()->info("0.1");
+		$this->getLogger()->info("=========================");
+		$this->players = new Config($this->getDataFolder()."Players.yml", Config::YAML);
+	}
 	
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) :bool{
+		$name = $sender->getName();
+		if(!$this->players->exists($name)){
+			$inv = $player->getInventory->getContents();
+			$this->players->set($name,$inv);
+			$this->players->save();
+			$player->getInventory()->clearAll();
+		}else{
+			$invv = $this->players->get($name);
+			$player->getInventory->setContents($invv);
+			$this->players->remove($name);
+			$this->players->save();
+			$inv = $player->getInventory->getContents();
+			$this->players->set($name,$inv);
+			$this->players->save();
+		}
+	}
+}
