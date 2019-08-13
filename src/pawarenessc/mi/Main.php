27b060b7dd39e816707f1d58540be24127e26706
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-14 06:11:04 +0900

    Create Main.php

---
+++ src/pawarenessc/mi/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace pawarenessc\mi;
+
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends pluginBase implements Listener{
+
+	public function onEnable(){
+	
